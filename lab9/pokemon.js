f36a34c2b02d974b78f3d2f93028ce1657072640
victorDel27 <victor.delgado@utp.ac.pa>
2023-07-02 22:38:33 -0500

    agregando lab9

---
+++ lab9/pokemon.js
@@ -0,0 +1,70 @@
+const { get } = require("./utils");
+const cache = require("./cache");
+module.exports = {
+  arrPokemonesCadenaEvolucion: [],
+
+  async obtenerInfoPokemon(nombrePokemon) {
+    const response = await get(`https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`);
+    const { abilities, name, height, weight, id } = response;
+    const cadenaEvolutiva = await this.obtenerDeEvolucion(id);
+    const cadenaPokemones = this.arregloCadenaEvolucion(cadenaEvolutiva);
+    return {
+      name,
+      id,
+      weight,
+      height,
+      abilities: abilities.map((ability) => ability.ability.name).join(", "),
+        cadenaEvolucion: cadenaPokemones.join(", "),
+    };
+  },
+  async obtenerDeEvolucion(id) {
+    const url = `https://pokeapi.co/api/v2/pokemon-species/${id}`;
+    const cadenaEvolucion = await get(url);
+    const { evolution_chain } = cadenaEvolucion;
+    const cadenaEvolucion2 = await get(evolution_chain.url);
+    const { chain } = cadenaEvolucion2;
+    return chain;
+  },
+
+  arregloCadenaEvolucion(chain) {
+    this.arrPokemonesCadenaEvolucion = [];
+    const {evolves_to,species: { name },} = chain;
+    this.arrPokemonesCadenaEvolucion.push(name);
+    this.recorrerArrEvolucion(evolves_to);
+    return this.arrPokemonesCadenaEvolucion;
+  },
+
+  recorrerArrEvolucion(evolves_to) {
+    evolves_to.forEach((element) => {
+      const {species: { name },} = element;
+      this.arrPokemonesCadenaEvolucion.push(name);
+      if (element.evolves_to.length > 0) {
+        this.recorrerArrEvolucion(element.evolves_to);
+      }
+    });
+  },
+
+  async setCache(pokemonId) {
+    const dataPokemon = await this.obtenerInfoPokemon(pokemonId);
+    const {id}  = dataPokemon;
+    if(!cache.cache[id]){
+        cache.fromCache = false;
+        cache.cache[id] = dataPokemon;
+        setTimeout(() => {
+            cache.fromCache = false;
+             cache.cache = {};
+        },300000);
+    }else{
+        cache.fromCache = true;
+        cache.cache[id];
+    }
+
+    return "ir a cache para ver el pokemon";
+  },
+
+  getCache() {
+    return cache;
+  }
+
+  
+};
